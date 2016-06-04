a77d832770679884a99d1940b9c5bcae72030f3c
raidel <rraidel89@gmail.com>
2016-06-04 00:45:08 -0400

    URL permisos

--- academica/urls.py
+++ academica/urls.py
@@ -2,6 +2,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
+from django.contrib.auth.decorators import login_required, permission_required
 
 
 from academica.views import PlanCreate, \
@@ -27,11 +28,11 @@ urlpatterns = patterns('',
                        url(r'^deveploment/', TemplateView.as_view(template_name="404.html")),
                        # url(r'^list/$', AlumnosList.as_view(), name='alumnos-list'),
                        # planEstudio
-                       url(r'planEstudio/add/$', PlanCreate.as_view(), name='planEstudio-add'),
-                       url(r'planEstudio/(?P<pk>[0-9]+)/$', PlanUpdate.as_view(), name='planEstudio-update'),
-                       url(r'planEstudio/list/$', PlanEstudioList.as_view(), name='list-planEstudio'),
+                       url(r'planEstudio/add/$',permission_required('users.permissions_administrador', login_url='login')( PlanCreate.as_view()), name='planEstudio-add'),
+                       url(r'planEstudio/(?P<pk>[0-9]+)/$',permission_required('users.permissions_administrador', login_url='login')(  PlanUpdate.as_view()), name='planEstudio-update'),
+                       url(r'planEstudio/list/$', permission_required('users.permissions_administrador', login_url='login')( PlanEstudioList.as_view()), name='list-planEstudio'),
                        # Alumno
-                       url(r'alumno/add/$', AlumnoCreate.as_view(), name='alumno-add'),
+                       url(r'alumno/add/$',permission_required('users.permissions_administrador', login_url='login')(  AlumnoCreate.as_view()), name='alumno-add'),
                        url(r'alumno/(?P<pk>[0-9]+)/$', AlumnoUpdate.as_view(), name='alumno-update'),
                        url(r'alumnore/(?P<pk>[0-9]+)/$', AlumnoReins.as_view(), name='alumnore-update'),
                        url(r'alumno/list/$', AlumnoList.as_view(), name='list-alumno'),
@@ -41,9 +42,9 @@ urlpatterns = patterns('',
                        url(r'alumno/add/addExtracurricular/$', ExtracurricularesCreate.as_view(), name='extra-add'),
                        url(r'^extracurricular/list$', ExtracurricularList.as_view(), name='list-extra'),
                        # grupos
-                       url(r'grupo/add/$', GrupoCreate.as_view(), name='grupo-add'),
-                       url(r'grupo/list/$', GruposList.as_view(), name='list-grupos'),
-                       url(r'grupo/(?P<pk>[0-9]+)/$', GrupoUpdate.as_view(), name='grupo-update'),
+                       url(r'grupo/add/$',permission_required('users.permissions_administrador', login_url='login')(  GrupoCreate.as_view()), name='grupo-add'),
+                       url(r'grupo/list/$',permission_required('users.permissions_administrador', login_url='login')(  GruposList.as_view()), name='list-grupos'),
+                       url(r'grupo/(?P<pk>[0-9]+)/$',permission_required('users.permissions_administrador', login_url='login')(  GrupoUpdate.as_view()), name='grupo-update'),
                        # horario
                        url(r'horario/add/$', HorarioCreate.as_view(), name='horario-add'),
                        url(r'horario/list/$', HorarioList.as_view(), name='list-horario'),
@@ -58,20 +59,20 @@ urlpatterns = patterns('',
                        url(r'calificacion/list/$', CalificacionList.as_view(), name='list-calificacion'),
                        url(r'califAlum/(?P<alumno_id>\d+)/$', CalificacionList.get_calificacionesbyAlumno,
                            name='califi-alumno'),
-                       url(r'myCalif/$', CalificacionList.get_my_calificaciones, name='my-califi'),
+                       url(r'myCalif/$',permission_required('users.permissions_estudiante', login_url='login')(  CalificacionList.get_my_calificaciones), name='my-califi'),
                        url(r'calificacion/update/(?P<pk>[0-9]+)/$', CalificacionesUpdate.as_view(), name='calificacion-update'),
                        # carrera
                        url(r'carrera/add/$', CarreraCreate.as_view(), name='carrera-add'),
                        url(r'carrera/list/$', CarreraList.as_view(), name='list-carrera'),
                        url(r'carrera/update/(?P<pk>[0-9]+)/$', CarreraUpdate.as_view(), name='carrera-update'),
                        # semestre
-                       url(r'ciclosemestral/add/$', CicloSemestralCreate.as_view(), name='semestre-add'),
-                       url(r'ciclosemestral/list/$', CicloSemestralList.as_view(), name='list-semestre'),
-                       url(r'ciclosemestral/update/(?P<pk>[0-9]+)/$', CicloSemestralUpdate.as_view(),
+                       url(r'ciclosemestral/add/$',permission_required('users.permissions_administrador', login_url='login')(  CicloSemestralCreate.as_view()), name='semestre-add'),
+                       url(r'ciclosemestral/list/$',permission_required('users.permissions_administrador', login_url='login')(  CicloSemestralList.as_view()), name='list-semestre'),
+                       url(r'ciclosemestral/update/(?P<pk>[0-9]+)/$',permission_required('users.permissions_administrador', login_url='login')(  CicloSemestralUpdate.as_view()),
                            name='semestre-update'),
                        url(r'^semestre-ajax/$', CicloSemestralList.SemestreAjax, name='semestre-ajax'),
 
-                       url(r'baja/$', BajaCreate.as_view(), name='baja-add'),
+                       url(r'baja/$',permission_required('users.permissions_administrador', login_url='login')( BajaCreate.as_view()), name='baja-add'),
 
                        url(r'biblioteca/add$', BibliotecaCreate.as_view(), name='biblio-add'),
                        url(r'biblioteca/list$', BibliotecaList.as_view(), name='list-biblio'),
@@ -88,17 +89,17 @@ urlpatterns = patterns('',
                        url(r'centrocomp/delete$', BajaConta.as_view(), name='baja-cont'),
                        url(r'centrocomp/(?P<pk>[0-9]+)/$', ContaUpdate.as_view(), name='cont-update'),
 
-                       url(r'encuesta/add/$', EncuestaCreate.as_view(), name='encuesta-add'),
-                       url(r'encuesta/list/$', EncuestaList.as_view(), name='list-encuesta'),
+                       url(r'encuesta/add/$',permission_required('users.permissions_administrador', login_url='login')(  EncuestaCreate.as_view()), name='encuesta-add'),
+                       url(r'encuesta/list/$',permission_required('users.permissions_administrador', login_url='login')(  EncuestaList.as_view()), name='list-encuesta'),
 
-                       url(r'estado/add/$', EstadoCreate.as_view(), name='estado-add'),
-                       url(r'estado/list/$', EstadoList.as_view(), name='list-estado'),
+                       url(r'estado/add/$',permission_required('users.permissions_administrador', login_url='login')(  EstadoCreate.as_view()), name='estado-add'),
+                       url(r'estado/list/$',permission_required('users.permissions_administrador', login_url='login')(  EstadoList.as_view()), name='list-estado'),
 
-                       url(r'municipio/add/$', MunicipioCreate.as_view(), name='municipio-add'),
-                       url(r'municipio/list/$', MunicipioList.as_view(), name='list-municipio'),
+                       url(r'municipio/add/$',permission_required('users.permissions_administrador', login_url='login')(  MunicipioCreate.as_view()), name='municipio-add'),
+                       url(r'municipio/list/$',permission_required('users.permissions_administrador', login_url='login')(  MunicipioList.as_view()), name='list-municipio'),
 
-                       url(r'aula/add/$', AulaCreate.as_view(), name='aula-add'),
-                       url(r'aula/list/$', AulaList.as_view(), name='list-aula'),
+                       url(r'aula/add/$',permission_required('users.permissions_administrador', login_url='login')(  AulaCreate.as_view()), name='aula-add'),
+                       url(r'aula/list/$',permission_required('users.permissions_administrador', login_url='login')(  AulaList.as_view()), name='list-aula'),
 
                        url(r'servicio/add/$', ServicioSocialCreate.as_view(), name='servicio-add'),
                        url(r'servicio/list/$', ServicioSocialList.as_view(), name='list-servicio'),
@@ -111,8 +112,8 @@ urlpatterns = patterns('',
                        url(r'tipob/add/$', TipoBecaCreate.as_view(), name='tbeca-add'),
                        url(r'tipob/list/$', TipoBecaList.as_view(), name='list-tbeca'),
 
-                       url(r'escuela/add/$', EscuelaCreate.as_view(), name='escuela-add'),
-                       url(r'escuela/list/$', EscuelaList.as_view(), name='list-escuela'),
+                       url(r'escuela/add/$',permission_required('users.permissions_administrador', login_url='login')(  EscuelaCreate.as_view()), name='escuela-add'),
+                       url(r'escuela/list/$',permission_required('users.permissions_administrador', login_url='login')(  EscuelaList.as_view()), name='list-escuela'),
                        url(r'^admin/', include(admin.site.urls)),
                        # url(r'^admin/', include('manager.urls')),
 
